6895c881658053b48c98d51fcbd955e20a0c75cc
Tuvshinjarga1 <65581854+Tuvshinjarga1@users.noreply.github.com>
2025-02-24 11:38:32 +0800

    Update api.js

--- frontend/skill-sharing-platform/src/services/api.js
+++ frontend/skill-sharing-platform/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/posts';
-
+const API_URL = 'https://social-platform-backend.onrender.com/api/posts';
+// const API_URL = 'http://localhost:5000/api/posts';
 export const fetchPosts = async () => {
   try {
     const response = await axios.get(API_URL);
